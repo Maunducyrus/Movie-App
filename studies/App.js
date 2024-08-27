7679df517432c15a6d97e26a9a47531d0f61433d
Maunducyrus <maunducyrus123@gmail.com>
2024-08-27 21:56:43 +0300

    Update App.js

--- studies/App.js
+++ studies/App.js
@@ -1,32 +1,32 @@
 
-// import logo from './logo.svg';
-// import { useState, useEffect } from 'react';
-import { useState } from 'react';
-import './App.css';
+// // import logo from './logo.svg';
+// // import { useState, useEffect } from 'react';
+// import { useState } from 'react';
+// import './App.css';
 
-const Person = (props) => {
-  return 
+// const Person = (props) => {
+//   return 
   
-}
-    // <>
-    // <h1>Name: {props.name}</h1>
-    // <h2>Last Name: {props.lastname}</h2>
-    // <h2>Age: {props.age}</h2>
-    // </>
+// }
+//     // <>
+//     // <h1>Name: {props.name}</h1>
+//     // <h2>Last Name: {props.lastname}</h2>
+//     // <h2>Age: {props.age}</h2>
+//     // </>
 
-function App() {
-const [counter, setCounter] = useState(0);
-  return (
-    <div className="App">
-      {/* <Person name="Waambua" lastname='Ben' age='21'/>
-      <Person name='Cecilia' lastname='Lucky' age='19'/> */}
+// function App() {
+// const [counter, setCounter] = useState(0);
+//   return (
+//     <div className="App">
+//       {/* <Person name="Waambua" lastname='Ben' age='21'/>
+//       <Person name='Cecilia' lastname='Lucky' age='19'/> */}
 
-      <button onClick={() => setCounter((prevCount) => prevCount -1)}>-</button>
-      <h1>{counter}</h1>
-      <button onClick={() => setCounter((prevCount) => prevCount +1)}>+</button>
+//       <button onClick={() => setCounter((prevCount) => prevCount -1)}>-</button>
+//       <h1>{counter}</h1>
+//       <button onClick={() => setCounter((prevCount) => prevCount +1)}>+</button>
 
-    </div>
-  );
-}
+//     </div>
+//   );
+// }
 
-export default App;
+// export default App;
